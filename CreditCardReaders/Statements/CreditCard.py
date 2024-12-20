b9f3ae609deed1e2c8674a6783659060618d4edd
Marcos Vinicius <85797701+strngrthnall@users.noreply.github.com>
2024-12-19 22:49:33 -0300

    Inicio de back-end para o dashboard

---
+++ CreditCardReaders/Statements/CreditCard.py
@@ -0,0 +1,28 @@
+from commonImports import *
+
+class CreditCard:
+  def __init__(self, lastDigits:int, bank:str, limit:float, closingDay:int, dueDay:int):
+    self.lastDigits = lastDigits
+    self.bank = bank
+    self.limit = limit
+    self.closingDay = closingDay
+    self.dueDay = dueDay
+    
+  @classmethod
+  def createCreditCard(cls, lastDigits:int, bank:str, limit:float, closingDay:int, dueDay:int):
+    return cls(lastDigits, bank, limit, dueDay, closingDay)
+
+  def toDict(self) -> Dict[str, Any]:
+    return{
+      'lastDigits': self.lastDigits,
+      'bank': self.bank,
+      'limit': self.limit,
+      'closingDay': self.closingDay,
+      'dueDay': self.dueDay
+    }
+    
+  @classmethod
+  def fromDict(cls, data: Dict[str, Any]) -> 'CreditCard':
+    
+    return cls(data['last_digits'], data['bank'], data['limit'], data['closing_day'], data['due_day'])
+
---
+++ config/commonImports.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+  import json
+  from datetime import datetime as dt
+  from typing import Dict, Any
